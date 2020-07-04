55d596991d7d61b73a5d5a16158e8f2d86f16744
Andi Hafiidh <andi.hafiidh@gmail.com>
2020-07-04 09:15:37 +0700

    Init

---
+++ precache-manifest.936b539d6cd87535094c14b1db2085f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "62dc88a5fdd62f1e62b6",
+    "url": "/js/chunk-vendors.52bfb5c9.js"
+  },
+  {
+    "revision": "635b0e1b8477037a2535",
+    "url": "/js/app.cc6bf7eb.js"
+  },
+  {
+    "revision": "49aece995574e6a7908a6a1216345a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62dc88a5fdd62f1e62b6",
+    "url": "/css/chunk-vendors.403d28f9.css"
+  },
+  {
+    "revision": "635b0e1b8477037a2535",
+    "url": "/css/app.e0447dae.css"
+  }
+];
